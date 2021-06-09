6714d1599fdec2aaf7acf8e09e7f76f55c4c8aa9
DuCheAnh <ducheanh1308@gmail.com>
2021-06-09 15:30:39 +0700

    bug fix for Pilot

--- Random/Pilot.py
+++ Random/Pilot.py
@@ -7,7 +7,6 @@ for i in range(m):
         x = int(input())
         mat[i].append(x)
 
-
 # xu ly deviation
 sub = False
 deviation = []
@@ -30,14 +29,19 @@ pilot_pos = [0]*len(deviation)  # 0= lai chinh, 1= lai phu
 count = 0
 while count < len(deviation)/2:
     pos = deviation.index(max(deviation))
+    if pos == 0:
+        deviation[pos] = 0
+        pilot_pos[pos] = 1
+        count += 1
     total_sub = 0
     for i in range(pos):
         if (pilot_pos[i] == 1):
             total_sub += 1
     if (total_sub < pos):
-        deviation[pos] = 0
         pilot_pos[pos] = 1
         count += 1
+    deviation[pos] = min(deviation)
+
 #
 index = 1
 for i in pilot_pos:
